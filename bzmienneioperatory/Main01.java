701faea8c17a979e0dd897a92b13d318eec98ba1
Karol Lukaszewski <karol.lukaszewski87@gmail.com>
2020-10-02 13:56:36 +0200

    Zmienne i operatory

---
+++ bzmienneioperatory/Main01.java
@@ -0,0 +1,13 @@
+package pl.coderslab.bzmienneioperatory;
+
+public class Main01 {
+
+	public static void main(String[] args) {
+		int int1 = 5;
+		char char2 = 'A';
+		long long3 = 123444;
+		double double4 = 12.6;
+		boolean boolean5 = true;
+	}
+
+}
